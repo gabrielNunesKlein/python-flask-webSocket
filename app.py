a306f0ec46ef95925e72f8dd3c27fdbb8ea5bfc8
Gabriel Klein <gabriel.klein@evolutionsoft.com.br>
2025-07-05 00:37:56 -0300

    Chore: db connection db and payment table

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify
+from repository.database import db
+from db_models.payment import Payment
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'SECRET_KEY_SOCKET'
+
+db.init_app(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
@@ -16,6 +23,6 @@ def confirmation_pix():
 def payment_pix_page(payment_id):
     return 'pagamento pix'
 
-if __name__ == "main":
+if __name__ == '__main__':
     app.run(debug=True)
 
